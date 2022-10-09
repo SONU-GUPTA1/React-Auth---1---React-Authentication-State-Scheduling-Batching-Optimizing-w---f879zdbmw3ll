e1d30a19a0918873c7f3690351eb75e90ff0a672
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-10-09 16:50:22 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
+// import React from 'react';
+
+// export const Home = () =>{
+
+//     return(
+//         <div id="home-page">
+//             Home page
+//         </div>
+//     )
+// }
+
 import React from 'react';
+import { useAuth } from '../components/loginAuth';
 
 export const Home = () =>{
-
+    const auth = useAuth();
     return(
         <div id="home-page">
-            Home page
+            <button id='logout-btn' onClick={() => auth.logout()}>Logout</button>
         </div>
     )
 }
