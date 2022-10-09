0ee49e71c68912bd3af246851ed6defbdccea1b0
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-10-09 16:53:25 +0530

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,33 @@
+// import React from 'react'
+// import { BrowserRouter } from 'react-router-dom'
+// import { Routes, Route } from 'react-router'
+// import { Signin } from './pages/Signin'
+// import { Home } from './pages/Home'
+// import { Signup } from './pages/Signup'
+// export const AppRoutes = () => {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path='/home' element={<Home />} />
+//                 <Route path='/signin' element={<Signin />} />
+//                 <Route path='/signup' element={<Signup />} />
+//             </Routes>
+//         </BrowserRouter>
+//     )
+// }
+
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router'
 import { Signin } from './pages/Signin'
 import { Home } from './pages/Home'
 import { Signup } from './pages/Signup'
+import RequireAuth from './components/requireAuth'
 export const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/home' element={<Home />} />
+                <Route path='/home' element={<RequireAuth><Home /></RequireAuth>} />
                 <Route path='/signin' element={<Signin />} />
                 <Route path='/signup' element={<Signup />} />
             </Routes>
